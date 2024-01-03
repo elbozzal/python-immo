2c2a8ef08fcdef68aaa1c4c778b46006cddb513a
elbozzal <42968843+elbozzal@users.noreply.github.com>
2024-01-03 17:15:23 +0100

    Update test.py
    Added env variable for future dockerisation

--- test.py
+++ test.py
@@ -1,19 +1,55 @@
+#Abstract : Créer un bot discord peu intelligent, et le dockerisé
+
 import discord
+from discord.ext import commands
 
-# Création de l'objet client avec les intents appropriés
-intents = discord.Intents.default()
-intents.members = True
-client = discord.Client(intents=intents)
-
+intents = discord.Intents.all()
+#client = discord.Client(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 # Code du bot
-@client.event
+
+@bot.event
 async def on_ready():
-    print('Bot prêt à fonctionner')
+        print('We have logged in as {0.user}'.format(bot))
 
-@client.event
+
+@bot.event
 async def on_message(message):
-    if message.content == 'test':
-        await message.channel.send('icule')
-
-# Lancement du bot
-client.run('')
+        if message.author == bot.user:
+                return
+#       if str(message.author.id) == "AMI":
+#               await message.channel.send('<:middle_finger:1112370247534657538>')
+        if str(message.author.id) == "AMI":
+                await message.add_reaction('\U0001F497')
+        if 'test' in message.content.lower():
+                if str(message.author.id) == "CHEF":
+                        await message.channel.send('icule! O grand chef vénéré')
+                else:
+                        await message.channel.send('icule!')
+        if 'donc' in message.content.lower():
+                if str(message.author.id) == "CHEF":
+                        await message.channel.send('et Kong! O grand chef vénéré')
+                else:
+                        await message.channel.send('et Kong!')
+        if 'fuck you' in message.content.lower():
+                if str(message.author.id) == "CHEF":
+                        await message.channel.send('fuck you! O grand chef vénéré<:middle_finger:1112370247534657538>')
+                else:
+                        await message.channel.send('<:middle_finger:1112370247534657538>')
+        if 'pouic' in message.content.lower():
+                if str(message.author.id) == "CHEF":
+                        await message.channel.send('pouic pouic! O grand chef vénéré')
+                else:
+                        await message.channel.send('pouic pouic!')
+        if 'allo' in message.content.lower():
+                if str(message.author.id) == "CHEF":
+                        await message.channel.send(' à l huile! O grand chef vénéré')
+                else:
+                        await message.channel.send('à l huile!')
+        if 'go' in message.content.lower():
+                if str(message.author.id) == "CHEF":
+                        await message.channel.send(' c est tipar! O grand chef vénéré')
+                else:
+                        await message.channel.send('c est tipar')
+        # Lancement du bot
+bot.run('TOKEN')
